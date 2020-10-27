3b56d7e810cfb2c8304b733668c0bf35ec76b5eb
MSI\moneo <jvr.moneo@gmail.com>
2020-10-27 12:32:02 +0100

    crear estructura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormTemplateComponent } from './shared/components/form-template/form-template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'template', component: FormTemplateComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'template'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
